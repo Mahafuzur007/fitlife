ed099180d816333a6d9cd579ada0dc0749efbd3e
Md. Mahafuzur Rahman <mahafuzurrahmanhridoy@gmail.com>
2024-03-17 16:12:59 +0600

    init

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navlinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+};
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+};
+
+addEventOnElem(navlinks, "click", closeNavbar);
+
+/**
+ * header & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backToBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backToBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backToBtn.classList.remove("active");
+  }
+});
